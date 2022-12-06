bb0f7b32f2fde9d1bdf8fdf33e31cc7e1a089865
Cristóvão Trevisan <cristovao.trevisan@gmail.com>
2022-12-06 17:34:34 -0300

    Use cache reload in page load

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,6 +1,6 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
-	const res = await fetch('/');
+	const res = await fetch('/', { cache: 'reload' });
 	const user = await res.json();
 	return { user };
 }
